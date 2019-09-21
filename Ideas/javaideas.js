4c78f2d0c41d960a73f377c6a9e83ae72c7d92d5
Dominick <Domdominick@hotmail.com>
2019-09-21 13:02:54 -0400

    updated

--- Ideas/javaideas.js
+++ Ideas/javaideas.js
@@ -1,103 +1,105 @@
-var config = {
-    apiKey: "AIzaSyCs3K5zwuOuS0odq89IpPLC7HnXTOcDqgI",
-    authDomain: "recent-user-with-all-use-e8e76.firebaseapp.com",
-    databaseURL: "https://recent-user-with-all-use-e8e76.firebaseio.com",
-    projectId: "recent-user-with-all-use-e8e76",
-    storageBucket: ""
-};
+ var firebaseConfig = {
+     apiKey: "AIzaSyB_u64MjpDHqA7cw3xQ4QCAnnlRvpcLfi8",
+     authDomain: "domm-a7373.firebaseapp.com",
+     databaseURL: "https://domm-a7373.firebaseio.com",
+     projectId: "domm-a7373",
+     storageBucket: "",
+     messagingSenderId: "651633677889",
+     appId: "1:651633677889:web:8731b3f75b269e2c6658c0"
+ };
 
-firebase.initializeApp(config);
+ firebase.initializeApp(firebaseConfig);
 
-var database = firebase.database();
+ var database = firebase.database();
 
 
-var user = {
-    username: //links to user input on database
-        password: //links to password on login
-        email: //user email for notifications
-        nameF: //user first name
-        nameL: //user last name
-        sex: //user sex
-        age: //user age
-        weight: //user weight
-        height: //user height
-        goal: //user weight/health goals updated based off database changes
-        track: [] //array of new weight to track progess
-}
-var BMI = 0 //updates based off user health stats from calculator
-var newUser = false;
-var login = false;
-$("#new-user").on("click", function() {
-    newUser = true;
-})
-$("#login-page").on("click", function() {
-    login = true;
-})
+ var user = {
+     username: //links to user input on database
+         password: //links to password on login
+         email: //user email for notifications
+         nameF: //user first name
+         nameL: //user last name
+         sex: //user sex
+         age: //user age
+         weight: //user weight
+         height: //user height
+         goal: //user weight/health goals updated based off database changes
+         track: [] //array of new weight to track progess
+ }
+ var BMI = 0 //updates based off user health stats from calculator
+ var newUser = false;
+ var login = false;
+ $("#new-user").on("click", function() {
+     newUser = true;
+ })
+ $("#login-page").on("click", function() {
+     login = true;
+ })
 
-if (newUser === true) {
-    CreateNewUser();
-} else if (login === true) {
-    LoginPage();
-} else {}
+ if (newUser === true) {
+     CreateNewUser();
+ } else if (login === true) {
+     LoginPage();
+ } else {}
 
-function CreateNewUser() {
+ function CreateNewUser() {
 
-    $("#submit").on("click", function NewUser() {
+     $("#submit").on("click", function NewUser() {
 
-        user.username = $("#username-input").val().trim();
-        user.password = $("#password-input").val().trim();
-        user.email = $("#email-input").val().trim();
-        user.nameF = $("#first-input").val().trim();
-        user.nameL = $("#last-input").val().trim();
-        user.sex = $("#sex-input").val().trim();
-        user.age = $("#age-input").val().trim();
-        user.weight = $("#weight-input").val().trim();
-        user.height = $("#height-input").val().trim();
-        user.goal = $("#goal-input").val().trim();
-        user.track[0] = $("#weight-input").val().trim();
+         user.username = $("#username-input").val().trim();
+         user.password = $("#password-input").val().trim();
+         user.email = $("#email-input").val().trim();
+         user.nameF = $("#first-input").val().trim();
+         user.nameL = $("#last-input").val().trim();
+         user.sex = $("#sex-input").val().trim();
+         user.age = $("#age-input").val().trim();
+         user.weight = $("#weight-input").val().trim();
+         user.height = $("#height-input").val().trim();
+         user.goal = $("#goal-input").val().trim();
+         user.track[0] = $("#weight-input").val().trim();
 
-        if (user.password === $("#passwordCheck-input").val().trim()) {
-            database.ref("/users").push({
+         if (user.password === $("#passwordCheck-input").val().trim()) {
+             database.ref("/users").push({
 
-                user: user.username,
-                password: user.password,
-                email: user.email,
-                first: user.nameF,
-                last: user.nameL,
-                sex: user.sex,
-                age: user.age,
-                weight: user.weight,
-                height: user.height,
-                goal: user.goal,
-                track: user.track
+                 user: user.username,
+                 password: user.password,
+                 email: user.email,
+                 first: user.nameF,
+                 last: user.nameL,
+                 sex: user.sex,
+                 age: user.age,
+                 weight: user.weight,
+                 height: user.height,
+                 goal: user.goal,
+                 track: user.track
 
-                //continue to next page
-            });
+                 //continue to next page
+             });
 
-        } else {
-            //error prompt, do not continue to next page
-        }
-    })
+         } else {
+             //error prompt, do not continue to next page
+         }
+     })
 
-}
+ }
 
-function LoginPage() {
-    $("#login").on("click", function LoginInput() {
+ function LoginPage() {
+     $("#login").on("click", function LoginInput() {
 
-        var username = $("#username-input").val().trim();
-        var password = $("#password-input").val().trim();
+         var username = $("#username-input").val().trim();
+         var password = $("#password-input").val().trim();
 
-        if (username === database.ref("/users").username || password === database.ref("/users").password) {
-            //lead to user's page
-        } else {
-            //prompt error "incorrect username/password"
-        }
+         if (username === database.ref("/users").username || password === database.ref("/users").password) {
+             //lead to user's page
+         } else {
+             //prompt error "incorrect username/password"
+         }
 
-    })
-    $("#create-new-user").on("click", CreateNewUser())
-}
+     })
+     $("#create-new-user").on("click", CreateNewUser())
+ }
 
-function BMI() {
-    BMI = (user.weight * 703) / user.height
-        //append to webpage
-}
+ function BMI() {
+     BMI = (user.weight * 703) / user.height
+         //append to webpage
+ }

02f720a15598cbcf7ea46ee54663717f45cc1ebf
Sam Levy <slevy239@gmail.com>
2019-09-24 20:55:44 -0400

    basic workout api

--- Ideas/workoutAPI.js
+++ Ideas/workoutAPI.js
@@ -2,19 +2,23 @@ $(document).ready(function () {
 
     
     var key = 'dafa9b25bfc440d631da35cffee0ae0be40de719'
-    var muscle = $("#");
+    var muscle = Math.floor(Math.Random() * 10);
+    var equipment = Math.floor(Math.Random() * 5);
     
     function displayWorkout() {
+        // can search by equiptment and muscle group
+        //possibly use random 
 
-    var queryURL = 'https://wger.de/api/v2/?format=api'
+    var queryURL = 'https://wger.de/api/v2/exercise/?equipment=' + equipment + '&format=json&muscles=' + muscle +'' 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function (response) {
         var results = response.data;
-    }) 
 
-    }
+    })
+}
+displayWorkout();
 
 })

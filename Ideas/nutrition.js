1f301a37b528b0ff91577c9c20472d11bdd32e68
ErikBoerma <eboerma@gmail.com>
2019-09-28 13:29:03 -0400

    updated nut page, added protein options and changed from appent to text so details display correctly

--- Ideas/nutrition.js
+++ Ideas/nutrition.js
@@ -1,45 +1,67 @@
 var apiKey = "c1af05bee1161ebc3a62cf3369b2b344"
 var apiID = "c6bee22a";
-var protien = {
- 1:"chicken",
- 2:"steak",
- 3:"fish",
-}
+var protien = "";
+
 // var startCals = 10; //range
 // var endCals = 5000;
+
 console.log(protien);
-
-
-
-// can search by equiptment and muscle group
-//possibly use random 
-
-
-var queryURL = "https://api.edamam.com/search?q=chicken&app_id=" + apiID + "&app_key=" + apiKey + "&from=0&to=3&calories=591-722&health=alcohol-free"
-
-
-
-function ajax() {
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            // $(".nutrition").append(results);
-
-            var newDiv = $("<div>")
-            console.log(response);
-            console.log(response.hits[0].recipe.label);
-            console.log(response.hits[0].recipe.ingredients);
-            console.log(response.hits[0].recipe.ingredients[0].text)
-            $(".label").append(response.hits[1].recipe.label);
-            $(".ingredients").append("<div>" + newDiv + response.hits[1].recipe.ingredients[0].text + "</div>");
-            $(".ingredients").append("<div>" + newDiv + response.hits[1].recipe.ingredients[1].text + "</div>");
-            $(".ingredients").append("<div>" + response.hits[1].recipe.ingredients[2].text + "</div>")
-
-
+    function displayRecipe() {
+    
+        var queryURL = "https://api.edamam.com/search?q=" + protien + "&app_id=" + apiID + "&app_key=" + apiKey + "&from=0&to=3&calories=591-722&health=alcohol-free"
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
         })
+            .then(function (response) {
+                // $(".nutrition").append(results);
+    
+                var newDiv = $("<div>")
+                console.log(response);
+                console.log(response.hits[0].recipe.label);
+                console.log(response.hits[0].recipe.ingredients);
+                console.log(response.hits[0].recipe.ingredients[0].text)
+                $(".label").text(response.hits[1].recipe.label);
+                $(".ingredients").text("<div>" + newDiv + response.hits[1].recipe.ingredients[0].text + "</div>");
+                $(".ingredients").text("<div>" + newDiv + response.hits[1].recipe.ingredients[1].text + "</div>");
+                $(".ingredients").text("<div>" + response.hits[1].recipe.ingredients[2].text + "</div>")
+    
+    
+            })
+    
+        }
+        displayRecipe();
+
+
+
+
+
+var queryURL = "https://api.edamam.com/search?q=" + protien + "&app_id=" + apiID + "&app_key=" + apiKey + "&from=0&to=3&calories=591-722&health=alcohol-free"
+
+$(document).on("click", "#submit1", function nutrition() {
+
+    protien = $("#APIsearch").val()
+    console.log(protien)
+    switch (parseInt(protien)) {
+        case "steak":
+            protien = "steak";
+            break;
+        case "chicken":
+            protien = "chicken";
+            break;
+        case "fish":
+            protien = "fish";
+            break;
+        case "veggie":
+            protien = "veggie";
+            break;
+        default:
+            break;
 
     }
-    ajax();
+
+    return displayRecipe(protien);
+
+});
+
